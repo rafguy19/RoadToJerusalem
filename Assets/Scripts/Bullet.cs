8e42793d4b17329fc0ed5a697d52daa3ee152d16
rafguy19 <ramanraif@gmail.com>
2023-08-15 14:39:26 +0800

    Fixed speed of arrow

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,18 +8,18 @@ public class Bullet : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;
-    [SerializeField]
     private PlayerController player;
     public float lifetime;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePos - transform.position;
         Vector3 rotation = transform.position - mousePos;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * player.arrowSpeed;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rot + 90);
         Destroy(gameObject, lifetime);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour
     public float arrowSpeed;
     [SerializeField]
     Slider bowPowerSlider;
+
     // Start is called before the first frame update
     void Start()
     {
