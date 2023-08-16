26852a4f9534a8b777b7a35c17d082fd506c9983
rafguy19 <ramanraif@gmail.com>
2023-08-16 16:41:04 +0800

    Added Arrow selector system

---
+++ Assets/Scripts/Player/ArrowWheelController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowWheelController : MonoBehaviour
+{
+    public Animator ar;
+    private bool arrowwheelelected = false;
+    public static int arrowID;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            arrowwheelelected = !arrowwheelelected;
+        }
+
+        if (arrowwheelelected)
+        {
+            ar.SetBool("OpenArrowWheel", true);
+        }
+        else
+        {
+            ar.SetBool("OpenArrowWheel", false);
+        }
+
+        switch (arrowID)
+        {
+            case 0: // No arrow selected
+                break;
+            case 1: // arrow selected
+                Debug.Log("Arrow");
+                break;
+            case 2: // fire arrow selected
+                Debug.Log("Fire Arrow");
+                break;
+            case 3: // holy arrow selected
+                Debug.Log("Holy Arrow");
+                break;
+            case 4: // unholy arrow selected
+                Debug.Log("Unholy Arrow");
+                break;
+            default:
+                break;
+        }
+    }
+}
