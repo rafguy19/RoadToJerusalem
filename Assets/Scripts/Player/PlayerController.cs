c000fb2d106cfe2fefad377b52b6ffe86e89cfde
ballsmacpherson <pangziyou@gmail.com>
2023-08-18 11:28:48 +0800

    bruh

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -51,7 +51,7 @@ public class PlayerController : MonoBehaviour
         UpdateDirection();
         UpdateFogClear();
 
-        if(playerCurrentWeapon.getWeapon() != null)
+        if (playerCurrentWeapon.getWeapon() != null)
         {
             if (playerCurrentWeapon.getWeapon().weaponType != EquipableItem.WeaponType.MELEE)
             {
@@ -79,75 +79,76 @@ public class PlayerController : MonoBehaviour
 
             moveDirection = new Vector2(moveX, moveY).normalized;
 
-        if (Input.GetMouseButton(0)) // Attacking
-        {
-            //if nothing is equiped (fist)
-            if(playerCurrentWeapon.getWeapon() == null)
+            if (Input.GetMouseButton(0)) // Attacking
             {
-                playerAttackController.AttackSelector(2);
-            }
-            else
-            {
-                switch (playerCurrentWeapon.getWeapon().weaponType)
+                //if nothing is equiped (fist)
+                if (playerCurrentWeapon.getWeapon() == null)
                 {
-                    case EquipableItem.WeaponType.BOW:
-                        playerAttackController.AttackSelector(1);
-                        break;
-                    case EquipableItem.WeaponType.MELEE:
-                        playerAttackController.AttackSelector(2);
-                        break;
-                    case EquipableItem.WeaponType.CROSSBOW:
-                        playerAttackController.AttackSelector(3);
-                        break;
+                    playerAttackController.AttackSelector(2);
+                }
+                else
+                {
+                    switch (playerCurrentWeapon.getWeapon().weaponType)
+                    {
+                        case EquipableItem.WeaponType.BOW:
+                            playerAttackController.AttackSelector(1);
+                            break;
+                        case EquipableItem.WeaponType.MELEE:
+                            playerAttackController.AttackSelector(2);
+                            break;
+                        case EquipableItem.WeaponType.CROSSBOW:
+                            playerAttackController.AttackSelector(3);
+                            break;
+                    }
                 }
             }
         }
-    }
 
-    void ApplyAnimation()
-    {
+        void ApplyAnimation()
+        {
 
-        if (rb.velocity != new Vector2(0,0))
-        {
-            ar.SetBool("Run", true);
+            if (rb.velocity != new Vector2(0, 0))
+            {
+                ar.SetBool("Run", true);
+            }
+            else
+            {
+                ar.SetBool("Run", false);
+            }
+
         }
-        else
+        void ApplyMovement()
         {
-            ar.SetBool("Run", false);
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        }
+        void UpdateDirection()
+        {
+            var dirH = Input.GetAxis("Horizontal");
+
+            if (dirH > 0f)
+            {
+                sr.flipX = false;
+            }
+            else if (dirH < 0f)
+            {
+                sr.flipX = true;
+            }
+
+        }
+
+        //player modifier
+        public void UpdateDamage(int newDmgValue)
+        {
+            atkDmg = newDmgValue;
+        }
+        void UpdateFogClear()
+        {
+            if (vfxRenderer != null)
+            {
+                vfxRenderer.SetVector3("PlayerPosition", transform.localPosition);
+            }
+
         }
 
     }
-    void ApplyMovement()
-    {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-    }
-    void UpdateDirection()
-    {
-        var dirH = Input.GetAxis("Horizontal");
-
-        if (dirH > 0f)
-        {
-            sr.flipX = false;
-        }
-        else if (dirH < 0f)
-        {
-            sr.flipX = true;
-        }
-        
-    }
-
-    //player modifier
-    public void UpdateDamage(int newDmgValue)
-    {
-        atkDmg = newDmgValue;
-    }
-    void UpdateFogClear()
-    {
-        if(vfxRenderer != null)
-        {
-            vfxRenderer.SetVector3("PlayerPosition", transform.localPosition);
-        }
-   
-    }
-    
 }
