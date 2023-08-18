f6e55b7ca42e8fe20ef31ee9b1c2f12a21731ebf
Ev3nts <93179644+BryantToh@users.noreply.github.com>
2023-08-18 17:11:20 +0800

    smoker is done but the smoke is not centered on player

--- Assets/Scripts/Enemy/SmokerZombie.cs
+++ Assets/Scripts/Enemy/SmokerZombie.cs
@@ -3,88 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SmokerZombie : MonoBehaviour
 {
-    public enum State
-    {
-        IDLE,
-        FOG,
-    }
-
-    [SerializeField]
-    public State currentState;
-    [SerializeField]
-    public List<GameObject> waypoints = new List<GameObject>();
-    private HunterZombieAI HunterZombieAI;
     private PlayerController playerController;
-    private float pounceDist = 4f;
-    private SpriteRenderer sr;
-    public int targetIndex;
-    private Rigidbody2D rb;
-    private ZombieAttack zombieAttack;
-    private bool isAttacking = false;
-    private float attackTimer;
-    private float attackTimerCountdown;
-    private float idleTime;
-    private float fogTime;
     public GameObject smoke;
 
+    private bool smokeSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        attackTimer = 1;
-        zombieAttack = GetComponentInChildren<ZombieAttack>();
-        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        HunterZombieAI = GetComponent<HunterZombieAI>();
-        rb = GetComponentInParent<Rigidbody2D>();
-        ChangeState(currentState);
+        playerController = GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentState == State.IDLE)
+        if (smoke.activeSelf)
         {
-            Idle();
-        }
-        else if (currentState == State.FOG)
-        {
+            //smoke.transform.position = playerController.transform.position;
             Fog();
-        }
-    }
-
-    private void ChangeState(State next)
-    {
-        if (next == State.IDLE)
-        {
-            idleTime = 0.0f;
-            GetComponent<HunterZombieAI>().enabled = false;
-        }
-        else if (next == State.FOG)
-        {
-            GetComponent<HunterZombieAI>().enabled = false;
-        }
-        currentState = next;
-    }
-
-    private void Idle()
-    {
-        idleTime += Time.deltaTime;
-
-        if (idleTime >= 15.0f)
-        {
-            ChangeState(State.FOG);
-            smoke.SetActive(true);
-            idleTime -= Time.deltaTime;
         }
     }
 
     private void Fog()
     {
-        fogTime += Time.deltaTime;
-
-        if (fogTime >= 10.0f)
-        {
-            smoke.SetActive(false);
-            ChangeState(State.IDLE);
-        }
+        smoke.SetActive(true);
     }
 }
