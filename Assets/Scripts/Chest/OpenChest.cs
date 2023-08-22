ba1e3670f12515a4f974c76b9bdf6da89695f773
ballsmacpherson <pangziyou@gmail.com>
2023-08-22 17:57:17 +0800

    chest open and deleting

--- Assets/Scripts/Chest/OpenChest.cs
+++ Assets/Scripts/Chest/OpenChest.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestOpen : MonoBehaviour
+public class OpenChest : MonoBehaviour
 {
-    public GameObject WeaponChest;
-    public GameObject FoodChest;
     private Animator ar;
-
-    
     // Start is called before the first frame update
     void Start()
     {
         ar = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collided)
     {
-        
+        if(collided.tag=="Player")
+        {
+            ar.Play("ChestOpen");
+            Destroy(gameObject, 6);
+        }
     }
 }
