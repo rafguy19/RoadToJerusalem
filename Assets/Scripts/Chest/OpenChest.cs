64f5faca0a5b93b6feeb86c9d25033eca3f3f522
ballsmacpherson <pangziyou@gmail.com>
2023-08-23 10:32:58 +0800

    chest rng

--- Assets/Scripts/Chest/OpenChest.cs
+++ Assets/Scripts/Chest/OpenChest.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class OpenChest : MonoBehaviour
 {
     private Animator ar;
+    private bool hasOpen;
+    public List<GameObject> ChestLoot = new List<GameObject>(); 
     // Start is called before the first frame update
     void Start()
     {
         ar = GetComponent<Animator>();
+        
     }
-
+    //void Update()
+    //{
+    //    RNGitemDrop();
+    //}
     private void OnTriggerEnter2D(Collider2D collided)
     {
         if(collided.tag=="Player")
@@ -19,4 +25,8 @@ public class OpenChest : MonoBehaviour
             Destroy(gameObject, 6);
         }
     }
+    //void RNGitemDrop()
+    //{
+
+    //}
 }
